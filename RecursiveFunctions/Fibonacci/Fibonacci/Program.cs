ecc3633b65d95458156e3f3f51b2bcd0f0df70e5
Fedor Lantukh <kulbaba685@gmail.com>
2021-01-17 15:13:12 +0200

    fibonacci

---
+++ RecursiveFunctions/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static int count = 0;
+        static Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int result = Fibonachi(2000);
+            Console.WriteLine("result " + result + " count " + count);
+        }
+
+        static int Fibonachi(int n)
+        {
+            count++;
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                if (!keyValuePairs.ContainsKey(n))
+                {
+                    keyValuePairs.Add(n, Fibonachi(n - 1) + Fibonachi(n - 2));
+                }
+
+                return keyValuePairs[n];
+            }
+        }
+    }
+}
